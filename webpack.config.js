328fcf652cf667452c032c712666b1a58376e5be
NapoliN <sakashita.y.aa@m.titech.ac.jp>
2022-02-06 22:31:28 +0900

    a

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, "src"),
+  context: path.join(__dirname, "docs"),
   entry: "./js/client.js",
   module: {
     rules: [{
@@ -19,11 +19,11 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "src"),
+      directory: path.join(__dirname, "docs"),
     }
   },
   output: {
-    path: __dirname + "/src/",
+    path: __dirname + "/docs/",
     filename: "client.min.js"
   },
   plugins: debug ? [] : [
